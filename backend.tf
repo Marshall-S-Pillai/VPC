94107db734cab1ca9cab19b13f478376c2697e9d
Marshall <143778910+Marshall-S-Pillai@users.noreply.github.com>
2024-12-18 21:49:54 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "powertool1107"
+    region         = "us-east-1"
+    encrypt        = true
+    versioning     = true
+    dynamodb_table = "powertool"
+  }
+}
