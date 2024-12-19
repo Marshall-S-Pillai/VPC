d9ef32fe605aa996d506ad8d0a43f8713c585863
Marshall <143778910+Marshall-S-Pillai@users.noreply.github.com>
2024-12-19 10:37:21 +0530

    Update variables.tf

--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -4,28 +4,27 @@ variable "vpc_id" {
   description = "The ID of the VPC."
 }
 
-Variable for subnet CIDR block
+
 variable "cidr_block" {
   type        = string
   description = "CIDR block for the subnet."
 }
 
-Variable for availability zone
+
 variable "availability_zone" {
   type        = string
   description = "The availability zone for the subnet."
 }
 
-Variable for public IP on launch
 variable "map_public_ip_on_launch" {
   type        = bool
   description = "Whether instances in the subnet receive a public IP on launch."
   default     = true
 }
 
-Variable for subnet name
+
 variable "subnet_name" {
   type        = string
   description = "The name of the subnet."
 }
-```
+
