a2fb9f918b50cb4e924aa9a8fb12e622ed094b85
Marshall <143778910+Marshall-S-Pillai@users.noreply.github.com>
2024-12-23 12:14:31 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "powertool9087"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "powertool"
