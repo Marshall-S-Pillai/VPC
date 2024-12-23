ef69cd17a0e4cf9761b17a43470080d45fa91533
Marshall <143778910+Marshall-S-Pillai@users.noreply.github.com>
2024-12-23 12:42:05 +0530

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,1 +1,18 @@
+resource "aws_security_group" "lab2" {
+  name        = "lab2-security-group"
+  description = "Security group for lab2"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
