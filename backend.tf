f03c2028ea873a74420e00df6d91912d44ad7a4f
Marshall <143778910+Marshall-S-Pillai@users.noreply.github.com>
2025-07-26 12:50:53 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "vpc/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "powertool"
+    use_lockfile   = true 
   }
 }
